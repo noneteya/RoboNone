290d641db5517ab34179cb35ea2b2d448ebb3db2
noneteya <45062623+noneteya@users.noreply.github.com>
2019-03-22 09:02:48 +0900

    feat: Sudoコマンドの実装

---
+++ cogs/moderation.py
@@ -0,0 +1,33 @@
+import argparse
+import copy
+import datetime
+import re
+import shlex
+from typing import Union
+import time
+
+import discord
+from discord.ext import commands
+
+
+class Arguments(argparse.ArgumentParser):
+    def error(self, message):
+        raise RuntimeError(message)
+
+
+def setup(bot):
+    bot.add_cog(Moderation(bot))
+
+
+class Moderation:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(hidden=True)
+    async def sudo(self, ctx, who: Union[discord.Member, discord.User], *, command: str):
+        """Run a command as another user."""
+        msg = copy.copy(ctx.message)
+        msg.author = who
+        msg.content = ctx.prefix + command
+        new_ctx = await self.bot.get_context(msg, cls=type(ctx))
+        await self.bot.invoke(new_ctx)
