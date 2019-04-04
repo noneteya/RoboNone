5f02973ad3df881c0be641f3228e14811422398a
和泉田済人 <sumito@izumita.com>
2019-04-04 18:42:46 +0900

    なぜか何回も使うのにHandlerだったのを直した DBManagerの使い方を変更した 新しい承認の方法を作った roles.pyの改修をした cmdではわからないのでsqlに変更 approvalをbot主なら作成可能に

--- cogs/approval.py
+++ cogs/approval.py
@@ -1,91 +1,134 @@
 import discord
 from discord.ext import commands
-import typing
 import asyncio
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+
+from .utils.database import Handler, DBManager
+
+create_table = """
+    CREATE TABLE approval IF NOT EXISTS(
+guild_id int,
+reaction_channel_id int,
+approval_channel_id int,
+wait_people_numbers int
+);
+""".replace("\n", "")
+
+if not os.getenv("ON_SERVER"):
+    # ローカルで走らせる場合
+    env_path = Path('./..') / '.env.local'
+    load_dotenv(dotenv_path=env_path)
+
+DB_URL = os.getenv("DATABASE_URL")
+admins = []
 
 
 def setup(bot):
     bot.add_cog(Approval(bot))
 
 
-def has_no_roles(ctx):
-    return len(ctx.message.author.roles) == 1
-
-
-def has_prospect(member):
-    for role in member.roles:
-        if role.name == 'prospect':
-            return True
+def is_everyone_only(author):
+    return len(author.roles) == 1
 
 
 class Approval:
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
+        global admins
         self.bot = bot
+        self.waiting = {}
+        self.manager = DBManager(DB_URL)
+        # なければ作成
+        self.manager.do(Handler(processing='execute', sql=create_table, fetch=False))
+        self.admins = [476172941692895232, 212513828641046529, bot.owner_id]
+        admins = self.admins
 
-    async def on_ready(self):
-        pass
+    @commands.command()
+    @commands.check(lambda ctx: ctx.author.id in admins)
+    async def create_setting(self, ctx, reaction_channel: discord.TextChannel, approval_channel: discord.TextChannel):
+        # DMの場合無視
+        if not ctx.guild:
+            return
+        sql = f"INSERT INTO approval VALUES ({ctx.guild.id}, {reaction_channel.id}, {approval_channel.id});"
+        self.manager.do(Handler(processing='execute', sql=sql, fetch=False))
 
-    async def on_raw_reaction_add(self, payload):
+    def get_guild_setting(self, guild_id: int):
+        result = self.manager.do(Handler(processing="execute",
+                                         cmd=f"SELECT * FROM approval WHERE guild_id = {guild_id}"))
+        if not result:
+            return []
+        return list(result[0])
+
+    def is_admin(self, _id):
+        return _id in self.admins
+
+    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
         emoji = payload.emoji
         channel = self.bot.get_channel(payload.channel_id)
         message = await channel.get_message(payload.message_id)
-        if message.guild:
-            member = message.guild.get_member(payload.user_id)
-        else:
+        guild = self.bot.get_guild(payload.guild_id)
+        member = guild.get_member(payload.user_id)
+        setting = self.get_guild_setting(guild.id)
+        # ここからチェック
+        #botの場合無視
+        if member.bot:
+            return
+        # settingが空の場合無視
+        if not setting:
+            return
+        # 絵文字が\U00002705でない場合無視
+        if not emoji == "\U00002705":
+            return
+        # チャンネルが規約チャンネル以外の場合無視
+        if channel.id != setting[1]:
+            # 自分の承認のやつに反応できないように
+            if channel.id == setting[2]:
+                if is_everyone_only(member):
+                    await message.remove_reaction(emoji, member)
+                    return
+
+            return
+        # ユーザーがeveryone以外の権限を持っていたら無視
+        if not is_everyone_only(member):
             return
 
-        if member == self.bot.user or not has_prospect(message.author):
-            return
+        # DMに承認を待つように送る、ダメであればチャンネルに送る
+        embed = discord.Embed(title="やさしいかくめいラボ 2.0 運営",
+                              description="承認要求を受理しました。承認まで今しばらくお待ちください。",
+                              color=discord.Color(0x00bfff))
+        try:
+            await member.send(embed=embed)
+        except discord.DiscordException:
+            c = guild.get_channel(setting[2])
+            await c.send(embed=embed)
 
-        if member == message.author or has_prospect(member) or len(member.roles) == 1:
-            await message.remove_reaction(emoji, member)
-            return
+        # 承認を待つ関数を呼ぶ
+        # create_taskをすることで一旦処理を終わらせる
+        self.bot.loop.create_task(self.wait_approval(member, guild))
 
-        if emoji.name == "✅":
+    async def wait_approval(self, member: discord.Member, guild: discord.Guild):
+        """3人の承認を待つ"""
+        setting = self.get_guild_setting(guild.id)
+        # 承認用メッセージを作成
+        channel = guild.get_channel(setting[2])
+        embed = discord.Embed(title=f"{member.display_name}さんの承認要求を受理しました",
+                              description="承認要求を承認してくださる方は\U00002705を押してください。"
+                                          "押されなかった場合は10分後に自動的に拒否されます。")
+        message = await channel.send(embed=embed)
+        await message.add_reaction("\U00002705")
 
-            check_mark_reaction = None
-            for reaction in message.reactions:
-                if reaction.emoji == "✅":
-                    check_mark_reaction = reaction
-
-            if check_mark_reaction and check_mark_reaction.count > 3:
-                role = discord.utils.find(lambda m: m.name == 'player', member.guild.roles)
-                await message.author.add_roles(role)
-                role = discord.utils.find(lambda m: m.name == 'prospect', member.guild.roles)
-                await message.author.remove_roles(role)
-                await message.channel.send(f"{message.author.mention} あなたは承認されました！")
-                await message.delete()
-            else:
-                m = await message.channel.send(f"{member.mention} 申請を承認しました")
-                await asyncio.sleep(3)
-                await m.delete()
-
-        elif emoji.name == "❎":
-
-            x_mark_reaction = None
-            for reaction in message.reactions:
-                if reaction.emoji == "❎":
-                    x_mark_reaction = reaction
-
-            if x_mark_reaction and x_mark_reaction.count > 3:
-                await message.channel.send(f"{message.author.mention} 申請が否認されました")
-                await message.delete()
-            else:
-                m = await message.channel.send(f"{member.mention} 申請を否認しました")
-                await asyncio.sleep(5)
-                await m.delete()
-
-        else:
-            await message.remove_reaction(emoji, member)
-
-    @commands.command()
-    @commands.check(has_no_roles)
-    async def agree(self, ctx):
-        """承認待ちができます"""
-        role = discord.utils.find(lambda m: m.name == 'prospect', ctx.guild.roles)
-        await ctx.author.add_roles(role)
-        await ctx.message.add_reaction("✅")
-        await ctx.message.add_reaction("❎")
-        m = await ctx.send(f"{ctx.author.mention} 承認待ちユーザーになりました！")
-        await asyncio.sleep(5)
-        await m.delete()
+        # 2分ごとにチェック
+        for t in range(5):
+            # キャッシュから取ると更新されないので新しくメッセージを取る
+            check_channel = self.bot.get_channel(setting[2])
+            check_message = await check_channel.get_message(message.id)
+            for reaction in check_message.reacions:
+                if reaction.emoji == "\U00002705":
+                    if reaction.count >= 4:
+                        await channel.send(f"{member.mention} さんの承認要求が許可されました！")
+                        role = discord.utils.find(lambda m: m.name == 'player', guild.roles)
+                        # 言わせてもらおう
+                        # 元の処理何？なんで役職を消す処理なんてしてんのじゃあなんで二つ以上の役職持ってるのにhasnt_any_rolesあんの
+                        await member.add_roles(role)
+        await channel.send(f"{member.mention} さんの承認要求は拒否されました。")
--- cogs/utils/database.py
+++ cogs/utils/database.py
@@ -1,27 +1,55 @@
 import psycopg2
 
-class DBHandler:
-    def __init__(self, db_url):
+
+class Handler:
+    def __init__(self, table_name=None, processing=None, *args, **kwargs):
+        self.table_name = table_name
+        self.processing = processing
+        self.args = args
+        self.kwargs = kwargs
+
+
+class DBManager:
+    def __init__(self, db_url, debug=False):
         self.db_url = db_url
         self.conn = None
+        self.debug = debug
 
     def get_connection(self):
         return psycopg2.connect(self.db_url)
 
-    def execute(self, cmd, fetch=True):
+    def do(self, handler: Handler):
+        """handler.processingで見分ける"""
+        if handler.processing == "execute":
+            return self._execute(*handler.args, **handler.kwargs)
+        elif handler.processing == "fetch_column":
+            return self.fetch_column(handler.table_name, *handler.args, **handler.kwargs)
+        elif handler.processing == "insert":
+            return self.insert(handler.table_name, *handler.args, **handler.kwargs)
+        elif handler.processing == "delete":
+            return self.delete(handler.table_name, *handler.args, **handler.kwargs)
+        elif handler.processing == "update":
+            return self.update(handler.table_name, *handler.args, **handler.kwargs)
+        else:
+            return None
+
+    def _execute(self, sql, fetch=True):
+        output = []
         self.conn = self.get_connection()
         cur = self.conn.cursor()
-        cur.execute(cmd)
+        cur.execute(sql)
         if fetch:
             data = cur.fetchall()
-            output = []
             for i in data:
-                output.append(list(i)[0])
+                # output.append(list(i)[0]) <- 言いたくないけどクソ処理　関数の名前詐欺してるよ
+                # executeなら結果全部返すようにしろよ(言葉遣い汚くてすまん)
+                output.append(i)
         cur.close()
 
         return output
 
-    def fetch_column(self, column, table, where=None, fetch=True):
+    def fetch_column(self, table, column, where=None, fetch=True):
+        output = []
         self.conn = self.get_connection()
         cur = self.conn.cursor()
         if where is not None:
@@ -31,19 +59,19 @@ class DBHandler:
 
         if fetch:
             data = cur.fetchall()
-            output = []
             for i in data:
                 output.append(list(i)[0])
         cur.close()
 
         return output
 
-    def insert(self, data, column, table):
-        columns = f"{column}".replace("'","")
+    def insert(self, table, data, column):
+        columns = f"{column}".replace("'", "")
         datas = f"{data}".replace("'", "")
 
         cmd = f"INSERT INTO {table} {columns} VALUES {datas};"
-        print(cmd)
+        if self.debug:
+            print(cmd)
 
         self.conn = self.get_connection()
         cur = self.conn.cursor()
@@ -51,7 +79,7 @@ class DBHandler:
         self.conn.commit()
         cur.close()
 
-    def delete(self, data, column, table):
+    def delete(self, table, data, column):
         columns = f"{column}".replace("'", "")
         datas = f"{data}".replace("'", "")
 
@@ -63,9 +91,7 @@ class DBHandler:
         self.conn.commit()
         cur.close()
 
-    def update(self, data, column, table, where=None):
-        columns = f"{column}".replace("'", "")
-        datas = f"{data}".replace("'", "")
+    def update(self, table, data, column, where=None):
 
         if where is not None:
             cmd = f"UPDATE {table} SET {column} = {data} WHERE {where}"
